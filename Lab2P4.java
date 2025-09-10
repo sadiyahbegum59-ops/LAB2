2aa9633f53d39fcd08365a9d756ba4d77d3e4327
sadiyahbegum59-ops <sadiyahbegum59@gmail.com>
2025-09-10 12:29:31 +0530

    Add files via upload

---
+++ Lab2P4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Lab2P4{
+public static void main (String...args){
+Scanner sc = new Scanner(System.in);
+int x = sc.nextInt();
+int y = sc.nextInt();
+System.out.println((x&y)+","+(x/y)+","+(x^y));
+
+
+
+
+}
+
+
+
+}
